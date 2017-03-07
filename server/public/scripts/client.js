ae5dc8183c348beab54f31b06f8676fcfac21b76
Paige Bystrom <pdbystrom@gmail.com>
2017-03-07 13:41:29 -0600

    appending all books to dom via angular

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,34 @@ var app = angular.module('BookApp', []);
 
 app.controller('BookController', ['$http', function($http) {
   console.log('BookController is loaded');
+  var self = this;
+  self.bookList = [];
+  self.newBook = {};
+
+getBooks();
+
+  function getBooks() {
+    $http({
+      method: 'GET',
+      url: '/books'
+    }).then(function(response) {
+      console.log(response.data);
+      self.bookList = response.data;
+    });
+  }
+
+self.addBook = function() {
+  $http({
+    method: 'POST',
+    url: '/books/new',
+    data: self.newBook
+  }).then(function(response) {
+    console.log(response);
+    getBooks();
+    self.newTask = {};
+  }, function errorCallback(response) { // error handling
+    console.log(response);
+  });
+};
+
 }]); // end app.controller
